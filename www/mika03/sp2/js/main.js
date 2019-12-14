f2b606d8e56d2f5f5df5e9b086bed0d5676524b1
mikovaa <58627321+mikovaa@users.noreply.github.com>
2019-12-14 15:29:50 +0100

    mika03-sp2
    Link to application: https://eso.vse.cz/~mika03/sp2/

---
+++ www/mika03/sp2/js/main.js
@@ -0,0 +1,35 @@
+
+/** 
+ * odeslat()
+ * @text - text emailu (obsah)
+ * @odesilatel - emailová adresa odesílatele
+ * @prijemce - příjemce emailu
+ * @predmet - předmět
+ * 
+ * odešle parametry přes AJAX na API
+*/
+function odeslat(text, odesilatel, prijemce, predmet){
+
+}
+/**
+ * pozdrav()
+ * @jmeno - jméno přihlášeného uživatele
+ * 
+ * vygeneruje pozdrav se jménem přihlášeného uživatele
+ */
+function pozdrav(jmeno){
+    if (jmeno==null) jmeno="uzivatel";
+    return "Zdravím, " + jmeno;
+}
+/**
+ * validovat()
+ * @text - text emailu (obsah)
+ * @odesilatel - emailová adresa odesílatele
+ * @prijemce - příjemce emailu
+ * @predmet - předmět
+ * 
+ * Kontrola validity vstupů
+ */
+function validovat(text, odesilatel, prijemce, predmet){
+
+}
