b2db145031f1178b8567deac3dc18ab52bdc7ff8
umlt00 <58665010+umlt00@users.noreply.github.com>
2019-12-15 23:46:18 +0100

    umlt00 - sp2
    https://eso.vse.cz/~umlt00/sp2/index.html

---
+++ www/umlt00/sp2/assets/main.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    var canvas = $('#canvas');
+    var head = $('#head');
+    var score = $('#score');
+    var speed_span = $('#speed');
+    var restart_btn = $('#restart_btn');
+})
+speed=10;
+a=0;
+b=100;
+run=0;
+y = -300;
+score=0;
+g = 2;
+checkbayqua=1;
+function myFunction(event) {
+    var x = event.keyCode;
+}
