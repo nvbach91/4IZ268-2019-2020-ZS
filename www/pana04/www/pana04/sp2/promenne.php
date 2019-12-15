3863c0fc5b2844dd237e2cdaa948ad52de281d66
pana04 <58917878+pana04@users.noreply.github.com>
2019-12-15 22:52:16 +0100

    Add files via upload

---
+++ www/pana04/www/pana04/sp2/promenne.php
@@ -0,0 +1,13 @@
+<?php
+
+  // Localhost
+  $servername = "mysql:host=localhost;dbname=STUDOBLOG; charset=utf8";
+  $username = "root";
+  $password = "APmyPHpAdmin2019";
+
+  // Hostinger
+   // $servername = "mysql:host=localhost;dbname=u713258664_gpro; charset=utf8";
+   // $username = "u713258664_ap";
+   // $password = "foZder-9votwe-hibcez";
+   // $db = "u713258664_gpro";
+?>
