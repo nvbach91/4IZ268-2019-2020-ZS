46931242ac7df185eb5accdc88fd11c5eafa159d
MagnificientLlama <37192877+MagnificientLlama@users.noreply.github.com>
2019-12-28 12:51:31 +0100

    zemp02-sp2
    Update code for moving, taking, and changing turns

--- www/zemp02/sp2/js/main.js
+++ www/zemp02/sp2/js/main.js
@@ -1,12 +1,26 @@
 /**
- * Memorama, Match Match, Match Up, Memory, Pelmanism, Shinkei-suijaku, Pexeso or simply Pairs
+ * Checkers
  */
 var selected = [];
+var moving = [];
+var whiteTurn = true;
+var whitePieces = 12;
+var blackPieces = 12;
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * Function for clearing selected class.
+ */
+function clearSelected() {
+    selected.forEach(element => {
+        element.classList.remove('selected');
+    });
 }
 
+/**
+ * Function creating rows of cells and squares on gameboard.
+ * @param {int} rowNumber number deciding row position in gameboard.
+ */
 function createRow(rowNumber) {
     let row = document.createElement('div');
     row.classList.add('gamerow');
@@ -16,6 +30,11 @@ function createRow(rowNumber) {
     document.getElementById('gameboard').appendChild(row);
 }
 
+/**
+ * Function to create checker pieces on given board cell.
+ * @param {string} type Decides what color created piece will have.
+ * @param {Object} square Determines on what cell piece will be created.
+ */
 function createChecker(type, square) {
     let checker = document.createElement('div');
     checker.classList.add('checker')
@@ -31,9 +50,14 @@ function createChecker(type, square) {
     square.appendChild(checker);
 }
 
+/**
+ * Function creating cells on gameboard
+ * @param {int} rowNumber y position on gameboard.
+ * @param {int} squareNumber x position on gameboard.
+ */
 function createSquare(rowNumber, squareNumber) {
     let square = document.createElement('div');
-    let id = (rowNumber).toString() + '-' + (squareNumber).toString();
+    let id = (squareNumber).toString() + '-' + (rowNumber).toString();
     square.id = id;
     square.classList.add('square')
     if ((rowNumber + squareNumber) % 2 === 1) {
@@ -46,22 +70,32 @@ function createSquare(rowNumber, squareNumber) {
 
 
     }
-    square.addEventListener('click', async function () {
+    square.addEventListener('click', function () {
         if ((square.classList.contains('gamecell') && (square.hasChildNodes()))) {
-            square.classList.add('selected');
             if (selected.length > 0) {
-                selected.forEach(element => {
-                    element.classList.remove('selected');
-                });
+                clearSelected();
+                if (selected[0] === square) {
+                    selected = [];
+                    moving = [];
+                    return square;
+                }
                 selected = [];
-            }
-            selected.push(square);
-            selectBasicMove(square);
-        } else {
-            selected.forEach(element => {
-                element.classList.remove('selected');
-            });
+                moving = [];
 
+            }
+            if((square.firstChild.classList.contains('white-player')&&whiteTurn)||(square.firstChild.classList.contains('black-player')&&! whiteTurn)){
+            square.classList.add('selected');
+            moving.push(square.firstChild);
+            selected.push(square);
+            showBasicMove(square);}
+        } else if (square.classList.contains('selected')) {
+            doBasicMove(square);
+            clearSelected();
+            selected = [];
+        } else {
+            clearSelected();
+            selected = [];
+            moving=[];
         }
     });
     return square;
@@ -69,46 +103,89 @@ function createSquare(rowNumber, squareNumber) {
 
 }
 
-function selectBasicMove(square) {
-    let row = parseInt(square.id.substr(0, 1), 10);
-    let column = parseInt(square.id.substr(2, 1), 10);
+/**
+ * Function to get coordinates of chosen gamecell around coordinates decided by input values.
+ * @param {int} row - Row coordinate.
+ * @param {int} column - Column coordinate.
+ * @param {boolean} checkLeft - Deciding which side(horizontally) of square should be checked.
+ * @param {boolean} checkUp  - Deciding which side(vertically) of square should be checked.
+ */
+function getSurroundingSquare(row, column, checkLeft, checkUp) {
+    checkLeft ? column-- : column++;
+    checkUp ? row-- : row++;
+    let id = column.toString() + '-' + row.toString();
+    return document.getElementById(id);
+}
+
+/**
+ * Function selecting viable moves.
+ * @param {Object} sideSquare - Square to check.
+ * @param {boolean} pieceIsBlack - Boolean deciding type of moving piece.
+ */
+function SelectCorrectMoves(sideSquare, pieceIsBlack) {
+
+    if (sideSquare.firstChild === null) {
+        sideSquare.classList.add('selected');
+        selected.push(sideSquare);
+    } else if (sideSquare.firstChild.classList.contains('white-player') && pieceIsBlack) {
+        return true;
+    } else if (sideSquare.firstChild.classList.contains('black-player') && !pieceIsBlack) {
+        return true;
+    }
+    return false
+}
+
+/**
+ * Function to check all moves of a basic checker piece.
+ * @param {Object} square Object around which you are checking possible moves.
+ */
+function showBasicMove(square) {
+    let column = parseInt(square.id.substr(0, 1), 10);
+    let row = parseInt(square.id.substr(2, 1), 10);
+    selected.push(square);
     if (square.firstChild.classList.contains('black-player')) {
         if (row > 1) {
-            rowCheck = row - 1;
             if (column > 1) {
-                let idLeft = rowCheck.toString() + '-' + (column - 1).toString();
-                let squareLeft = document.getElementById(idLeft);
-                if (squareLeft.firstChild === null) {
-                    squareLeft.classList.add('selected');
-                    selected.push(squareLeft);
+                let sideSquare = getSurroundingSquare(row, column, true, true);
+                let pieceCheck = SelectCorrectMoves(sideSquare, true)
+                if (pieceCheck && row > 2 && column > 2) {
+                    let sideColumn = parseInt(sideSquare.id.substr(0, 1), 10);
+                    let sideRow = parseInt(sideSquare.id.substr(2, 1), 10);
+                    let behindSquare = getSurroundingSquare(sideRow, sideColumn, true, true)
+                    SelectCorrectMoves(behindSquare, true);
                 }
             }
             if (column < 8) {
-                let idRight = rowCheck.toString() + '-' + (column + 1).toString();
-                let squareRight = document.getElementById(idRight);
-                if (squareRight.firstChild === null) {
-                    squareRight.classList.add('selected');
-                    selected.push(squareRight);
+                let sideSquare = getSurroundingSquare(row, column, false, true);
+                let pieceCheck = SelectCorrectMoves(sideSquare, true)
+                if (pieceCheck && row > 2 && column < 7) {
+                    let sideColumn = parseInt(sideSquare.id.substr(0, 1), 10);
+                    let sideRow = parseInt(sideSquare.id.substr(2, 1), 10);
+                    let behindSquare = getSurroundingSquare(sideRow, sideColumn, false, true)
+                    SelectCorrectMoves(behindSquare, true);
                 }
             }
         }
     } else if (square.firstChild.classList.contains('white-player')) {
         if (row < 8) {
-            rowCheck = row + 1;
             if (column > 1) {
-                let idLeft = rowCheck.toString() + '-' + (column - 1).toString();
-                let squareLeft = document.getElementById(idLeft);
-                if (squareLeft.firstChild === null) {
-                    squareLeft.classList.add('selected');
-                    selected.push(squareLeft);
+                let sideSquare = getSurroundingSquare(row, column, true, false);
+                let pieceCheck = SelectCorrectMoves(sideSquare, false)
+                if (pieceCheck && row < 7 && column > 2) {
+                    let sideColumn = parseInt(sideSquare.id.substr(0, 1), 10);
+                    let sideRow = parseInt(sideSquare.id.substr(2, 1), 10);
+                    let behindSquare = getSurroundingSquare(sideRow, sideColumn, true, false)
+                    SelectCorrectMoves(behindSquare, false);
                 }
             }
             if (column < 8) {
-                let idRight = rowCheck.toString() + '-' + (column + 1).toString();
-                let squareRight = document.getElementById(idRight);
-                if (squareRight.firstChild === null) {
-                    squareRight.classList.add('selected');
-                    selected.push(squareLeft);
+                let sideSquare = getSurroundingSquare(row, column, false, false);
+                let pieceCheck = SelectCorrectMoves(sideSquare, false)
+                if (pieceCheck && row < 7 && column < 7) {
+                    let sideColumn = parseInt(sideSquare.id.substr(0, 1), 10);
+                    let sideRow = parseInt(sideSquare.id.substr(2, 1), 10);
+                    let behindSquare = getSurroundingSquare(sideRow, sideColumn, false, false)
+                    SelectCorrectMoves(behindSquare, false);
                 }
             }
         }
@@ -116,6 +193,57 @@ function selectBasicMove(square) {
 
 }
 
+/**
+ * Function that is taking care of basic moves of pawns. Also removes pieces when taken.
+ * @param {Object} square - Destination square of chosen Piece.
+ */
+function doBasicMove(square) {
+    let gamePiece = moving[0];
+    let fromSquare = gamePiece.parentNode;
+    square.appendChild(gamePiece);
+    moving = [];
+
+    let fromColumn = parseInt(fromSquare.id.substr(0, 1), 10);
+    let fromRow = parseInt(fromSquare.id.substr(2, 1), 10);
+
+    let checkColumn = fromColumn;
+    let checkRow = fromRow;
+
+    let toColumn = parseInt(square.id.substr(0, 1), 10);
+    let toRow = parseInt(square.id.substr(2, 1), 10);
+
+    while (checkColumn != toColumn && checkRow != toRow) {
+        let checkId = checkColumn.toString() + '-' + checkRow.toString();
+        let checkPiece = document.getElementById(checkId).firstChild;
+        if (checkPiece != null) {
+            checkPiece.classList.contains('black-player') ? blackPieces-- : whitePieces--;
+            checkPiece.remove()
+        }
+        (fromColumn > toColumn) ? checkColumn-- : checkColumn++;
+        (fromRow > toRow) ? checkRow-- : checkRow++;
+    }
+    
+    whiteTurn?whiteTurn=false:whiteTurn=true;
+    clearSelected();
+    updateNumber();
+    
+}
+
+/**
+ * Function to update Score of players.
+ */
+function updateNumber() {
+    document.getElementById('blackPieces').innerText = 'Black player pieces: ' + blackPieces;
+    document.getElementById('whitePieces').innerText = 'White player pieces: ' + whitePieces;
+    let turn = whiteTurn?'White Player':'Black Player';
+    document.getElementById('playerTurn').innerText = 'Current turn: ' + turn;
+    if(blackPieces===0)alert('Congratulations white player, you won!');
+    if(whitePieces===0)alert('Congratulations black player, you won!');
+
+}
+
+
 for (let i = 1; i <= 8; i++) {
     createRow(i);
 }
+updateNumber();
