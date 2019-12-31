bd500059be15dc1094b70f7628a9adb27beee7fc
Martin Jelínek <martin.jelinek1@avast.com>
2019-12-31 02:59:47 +0100

    + script that handles logging user in and out added (forgotten in last commit)

--- www/jelm06/sp2/twitterapp/js/login.js
+++ www/jelm06/sp2/twitterapp/js/login.js
@@ -6,14 +6,19 @@ var isUserLogedIn = false;
  * Handles user's login to twitter to get OAuth token for API requests.
  */
 var login = function () {
+    loaderOnOff(true);
     console.log('searching for ' + hashtagValue);
     OAuth.initialize('RqDiAoEGjM7O8Eno938FoMuHTmk');
     OAuth.popup('twitter')
-        .done(function(result) {
+        .done(function (result) {
+            loaderOnOff(false);
             isUserLogedIn = true;
             userLoggedIn = result;
             $('#btn-login').unbind('click').html('LOGOUT').click(logout);
-
+            $('.before-login').css('display', 'none');
+            $('.after-login').css({'display': 'block', 'visibility': 'visible'});
+            $('.action-buttons').css('display', 'inline-block');
+            $(':header').css({'background-color': 'white', 'color': '#00acee'});
         })
         .fail(function (err) {
             alert("Unknown error: login failed.");
@@ -28,4 +33,6 @@ var logout = function () {
     userLoggedIn = '';
     isUserLogedIn = false;
     $('#btn-login').html('LOGIN').click(login);
+    $('.before-login').css('display', 'block');
+    $('.after-login').css({'display': 'none', 'visibility': 'hidden'});
 };
