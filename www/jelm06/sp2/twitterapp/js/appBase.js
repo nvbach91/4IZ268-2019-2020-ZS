6c784ccf0533d6561d2c5f1946dcc722d7345af4
Martin Jelínek <martin.jelinek1@avast.com>
2019-12-31 02:58:01 +0100

    + successfully displaying tweets using twitter API + loader added

--- www/jelm06/sp2/twitterapp/js/appBase.js
+++ www/jelm06/sp2/twitterapp/js/appBase.js
@@ -2,12 +2,13 @@ var hashtagLabelValue = "Your hashtag is: #";
 var teamsChosen = 0;
 var currentBtnSelected = "";
 var hashtagValue = "";
-
+var welcomeText = 'This is a twitter app made as final school work for course 4IZ268, University of Economics, Prague. To be able to use it, you need to login with your twitter account.';
+var isMenuDisplayed = true;
 
 /**
  * Handles all team-buttons and creates hashtags.
  */
-function addToCurrentHashtag() {
+var addToCurrentHashtag = function () {
     var tappedBtnText = $(this).text();
     // the user can select only two teams, there can not be two same teams in one hashtag
     if (teamsChosen < 2 && currentBtnSelected !== tappedBtnText) {
@@ -24,19 +25,53 @@ function addToCurrentHashtag() {
     $('#current-hashtag-value').text(hashtagLabelValue);
 }
 
+var clear = function () {
+    clearCurrentHashtag();
+    $('#tweets').empty();
+};
+
 /**
  * Clears all selected and saved values
  */
-function clearCurrentHashtag() {
+var clearCurrentHashtag = function () {
     currentBtnSelected = "";
     teamsChosen = 0;
     hashtagLabelValue = "Your hashtag is: #";
     $('#current-hashtag-value').text(hashtagLabelValue);
     hashtagValue = '';
+};
+
+/**
+ * Shows or hides loader.
+ *
+ * @param desiredState
+ */
+var loaderOnOff = function (desiredState) {
+    if (desiredState) {
+        $('#loader').append('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
+    } else {
+        $('#loader').empty();
+    }
+};
+
+var showAndHide = function () {
+    if (isMenuDisplayed === true) {
+        // hiding menu
+        $('.team-buttons').css('display', 'none');
+        $('#show-hide').find("i").removeClass("fa-angle-double-up").addClass("fa-angle-double-down");
+        $('#tweets').css('margin-top', '12em');
+        isMenuDisplayed = false;
+    } else {
+        // showing menu
+        $('.team-buttons').css('display', 'block');
+        $('#show-hide').find("i").removeClass("fa-angle-double-down").addClass("fa-angle-double-up");
+        $('#tweets').css('margin-top', '18em');
+        isMenuDisplayed = true;
+    }
 }
 
-init = function () {
-    $('#btn-clear').click(clearCurrentHashtag);
+var init = function () {
+    $('#btn-clear').click(clear);
     $('#btn-search').click(search);
     $('#btn-login').click(login);
 
@@ -60,6 +95,8 @@ init = function () {
     $('#btn-wat').click(addToCurrentHashtag);
     $('#btn-whu').click(addToCurrentHashtag);
     $('#btn-wlv').click(addToCurrentHashtag);
+
+    $('#show-hide').click(showAndHide);
 };
 
 $(document).ready(
--- www/jelm06/sp2/twitterapp/js/search.js
+++ www/jelm06/sp2/twitterapp/js/search.js
@@ -1,25 +1,54 @@
+var isResultDisplayed = false;
+
+var placeContent = function (status) {
+    $('.after-search').append('<div class="tweet">' +
+        '<div class="tweet-metadata">' +
+        status.created_at +
+        " By " +
+        '<a href="https://twitter.com/' + status.user.screen_name + '"' +
+        '>' + status.user.name +
+        '</a>' +
+        '</div>' +
+        '<div class="tweet-text">' +
+        status.full_text +
+        '</div>');
+};
+
+// TODO feature - include / exclude retweets
+// TODO feature idea - get most retweeted stats
+// TODO feature - chose number of tweets searched
+// TODO info div in header
 
 /**
  * Searches for Tweets based on @hashtagValue.
- * Basic Twitter search API provides only tweets younger than 7(?) days.
+ * Basic Twitter search API provides only tweets younger than 7 days.
  */
 var search = function () {
-    let queryURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + hashtagValue;
-    if(isUserLogedIn) {
+    var queryURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + hashtagValue + ' -filter:retweets' + '&count=100&tweet_mode=extended&;'
+    if (isUserLogedIn) {
+        if (isResultDisplayed) {
+            $('#tweets').empty();
+        }
+        loaderOnOff(true);
         console.log('searching for ' + hashtagValue)
         userLoggedIn.get(queryURL)
             .done(function (result) {
+                loaderOnOff(false);
+                isResultDisplayed = true;
+                console.log(result);
+                console.log(result.statuses[0].text);
                 console.log(result.statuses.length);
-                $('.tweets').css('visibility', 'visible');
-                $('.tweets').append('<p>Number of tweets found: </p>' + result.statuses.length);
+                $('.after-search')
+                    .css('visibility', 'visible')
+                result.statuses.forEach(function (status) {
+                    placeContent(status);
+                });
             })
-            .fail (function (err) {
+            .fail(function (err) {
                 console.log(err);
+                alert(err);
             });
-    } else {ff
+    } else {
         alert('You must be logged in!');
     }
 };
-
-
-
