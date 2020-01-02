a7ed81a63f7592aa6762b84b83cc0c819055b314
magdi98 <55974121+magdi98@users.noreply.github.com>
2020-01-02 19:32:02 +0100

    version 02
    Opravy a reakce na připomínky:
    Opraveny uvozovky na apostrofy, aplikování interpolace stringů na vynechaná místa
    Další úpravy a vylepšení:
    Byl změněn vzhled levého informačního panelu, informace o destinacích jsou nyní zobrazovány vertikálně (nikoliv horizontálně), dále byla upravena pozice tlačítka „smazat značky“
    Byly přidány nadpisy k destinacím a informace o teplotě místa
    Dále byl přidána informace o časovém posunu mezi dvěma místy, funkcionalita: po výběru obou míst se zobrazí pod informacemi o místech panel, který informuje o časovém posunu mezi zvolenými místy, při smazání míst panel opět zmizí, po přidání obou míst se opět zobrazí. Pokud nemá dostatek dat, informaci taktéž vypíše.

--- www/rydm00/sp2/js/main.js
+++ www/rydm00/sp2/js/main.js
@@ -1,14 +1,18 @@
 const App = {};
 
 App.init = () => {
-    App.button = $('.place-for-button');
-    App.locationButton = $('.my-location');
+    App.button = $('.delete-all-button');
+    App.locationButton = $('.my-location-button');
     App.latitude = 0;
     App.longitude = 0;
     App.weatherDestination1 = $('.weather-destination1');
     App.weatherDestination2 = $('.weather-destination2');
     App.timeDestination1 = $('.time-destination1');
     App.timeDestination2 = $('.time-destination2');
+    App.result = $('.results');
+    App.result.hide();
+    App.timezoneA = null;
+    App.timezoneB = null;
 };
 
 $(document).ready(() => {
@@ -17,15 +21,15 @@ $(document).ready(() => {
     function getLocation(callback) {
         if (navigator.geolocation) {
             var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
-                var user_position = {};
-                user_position.lat = position.coords.latitude;
-                user_position.lng = position.coords.longitude;
-                callback(user_position);
+                var userPosition = {};
+                userPosition.lat = position.coords.latitude;
+                userPosition.lng = position.coords.longitude;
+                callback(userPosition);
             }, function () {
-                var user_position = {};
-                user_position.lat = 49.7437572;
-                user_position.lng = 15.3386383;
-                callback(user_position);
+                var userPosition = {};
+                userPosition.lat = 49.7437572;
+                userPosition.lng = 15.3386383;
+                callback(userPosition);
                 alert('Nezjistil jsem tvou polohu, poloha byla nastavena do geografického středu ČR');
             });
         }
@@ -36,7 +40,7 @@ $(document).ready(() => {
         App.longitude = Object.values(lat_lng)[1];
 
         //add map
-        var map = new SMap(JAK.gel("map"), SMap.Coords.fromWGS84(App.longitude, App.latitude), 5);
+        var map = new SMap(JAK.gel('map'), SMap.Coords.fromWGS84(App.longitude, App.latitude), 5);
         map.addDefaultLayer(SMap.DEF_BASE).enable(); //basic layer of the map
         map.addDefaultControls();
 
@@ -60,10 +64,14 @@ $(document).ready(() => {
             layerMarker.removeAll();
             markersDeleteButton.hide();
             numberOfMarkers = 0;
-            $(".weather-destination1").text('');
-            $(".weather-destination2").text('');
-            $(".time-destination1").text('');
-            $(".time-destination2").text('');
+            $('.weather-destination1').text('');
+            $('.weather-destination2').text('');
+            $('.time-destination1').text('');
+            $('.time-destination2').text('');
+            App.timezoneA = null;
+            App.timezoneB = null;
+            $('.results').text('');
+            App.result.hide();
         });
 
         const locateButton = $('<button>');
@@ -73,7 +81,7 @@ $(document).ready(() => {
         locateButton.click(() => {
             var myCoords = SMap.Coords.fromWGS84(App.longitude, App.latitude);
             if (numberOfMarkers == 0) {
-                var marker1 = new SMap.Marker(myCoords, "marker1");
+                var marker1 = new SMap.Marker(myCoords, 'marker1');
                 layerMarker.addMarker(marker1);
 
                 const spinner = $('<div class="spinner"></div>');
@@ -86,7 +94,7 @@ $(document).ready(() => {
                 markersDeleteButton.show();
 
             } else if (numberOfMarkers == 1) {
-                var marker1 = new SMap.Marker(myCoords, "marker2");
+                var marker1 = new SMap.Marker(myCoords, 'marker2');
                 layerMarker.addMarker(marker1);
 
                 const spinner = $('<div class="spinner"></div>');
@@ -107,11 +115,11 @@ $(document).ready(() => {
 
         function clickMap(e) {
             var coords = SMap.Coords.fromEvent(e.data.event, map);
-            var coordinates = coords.toString().split(",");
+            var coordinates = coords.toString().split(',');
             var coordsLon = coordinates[0].slice(1, coordinates[0].length);
             var coordsLat = coordinates[1].slice(0, coordinates[1].length - 1);
             if (numberOfMarkers == 0) {
-                var marker1 = new SMap.Marker(coords, "marker1");
+                var marker1 = new SMap.Marker(coords, 'marker1');
                 layerMarker.addMarker(marker1);
 
                 const spinner = $('<div class="spinner"></div>');
@@ -124,7 +132,7 @@ $(document).ready(() => {
                 markersDeleteButton.show();
 
             } else if (numberOfMarkers == 1) {
-                var marker2 = new SMap.Marker(coords, "marker2");
+                var marker2 = new SMap.Marker(coords, 'marker2');
                 layerMarker.addMarker(marker2);
 
                 const spinner = $('<div class="spinner"></div>');
@@ -140,15 +148,15 @@ $(document).ready(() => {
 
         function addWeather(coords1, coords2, destination, spinner) {
             destination.append(spinner);
-            $.ajax({
-                url: 'http://api.weatherbit.io/v2.0/current?lat=' + coords1 + '&lon=' + coords2 + '&lang=cz&key=3a18e2d9578843c48342b988afbfae17',
-                method: 'GET',
-                dataType: 'json'
+            $.get({
+                url: `http://api.weatherbit.io/v2.0/current?lat=${coords1}&lon=${coords2}&lang=cz&key=3a18e2d9578843c48342b988afbfae17`
             }).done((data) => {
                 var weatherObject = data['data'][0]['weather'];
                 var otherInfo = data['data'][0];
                 let addedHTML = `
-                        <div class="weather-description">${otherInfo.city_name} (${otherInfo.country_code}) | ${weatherObject.description}</div>
+                        <div class="weather-place">${otherInfo.city_name} (${otherInfo.country_code})</div>
+                        <div class="weather-description">${weatherObject.description}</div>
+                        <div class="weather-temperature">${otherInfo.temp} °C</div>
                         <img class="weather-icon" src="https://www.weatherbit.io/static/img/icons/${weatherObject.icon}.png" alt="Ikonka počasí">
                         `;
                 destination.append(addedHTML);
@@ -159,23 +167,23 @@ $(document).ready(() => {
 
         function addTimezone(coords1, coords2, destination, spinner) {
             destination.append(spinner);
-            $.ajax({
-                url: 'http://api.timezonedb.com/v2.1/get-time-zone?key=BWIGLRXY65J4&format=json&by=position&lat=' + coords1 + '&lng=' + coords2,
-                method: 'GET',
+            $.get({
+                url: `http://api.timezonedb.com/v2.1/get-time-zone?key=BWIGLRXY65J4&format=json&by=position&lat=${coords1}&lng=${coords2}`
             }).done((data) => {
                 if (data.status === 'OK') {
-                    var UTCdifference = Math.floor(data.gmtOffset / 3600);
+                    var UTCtime = Math.floor(data.gmtOffset / 3600);
                     var date = data.formatted.toString();
-                    if (UTCdifference >= 0) {
+                    if (UTCtime >= 0) {
                         let addedHTML = `
-                        <div class="date-time">${moment(date, ["YYYY-MM-DD hh:mm:ss"], 'cs').format('LLL')}</div>
-                        <div class="gmt-offset">UTC+${UTCdifference}</div>
+                        <div class="date-time">${moment(date, ['YYYY-MM-DD hh:mm:ss'], 'cs').format('LLL')}</div>
+                        <div class="gmt-offset">UTC+${UTCtime}</div>
                         `;
                         destination.append(addedHTML);
+
                     } else {
                         let addedHTML = `
-                        <div class="date-time">${moment(date, ["YYYY-MM-DD hh:mm:ss"], 'cs').format('LLL')}</div>
-                        <div class="gmt-offset">UTC${UTCdifference}</div>
+                        <div class="date-time">${moment(date, ['YYYY-MM-DD hh:mm:ss'], 'cs').format('LLL')}</div>
+                        <div class="gmt-offset">UTC${UTCtime}</div>
                         `;
                         destination.append(addedHTML);
                     }
@@ -186,15 +194,43 @@ $(document).ready(() => {
                     `;
                     destination.append(addedHTML);
                 }
+                if (App.timezoneA === null) {
+                    App.timezoneA = UTCtime;
+                } else {
+                    App.timezoneB = UTCtime;
+                    App.result.show();
+                    var addedText;
+                    var difference;
+                    if (App.timezoneA === App.timezoneB) {
+                        addedText = `Časový posun mezi destinací A a B žádný není`;
+                        App.result.append(addedText);
+                    } else if (App.timezoneA > App.timezoneB) {
+                        difference = App.timezoneA - App.timezoneB;
+                    } else if (App.timezoneA < App.timezoneB) {
+                        difference = App.timezoneB - App.timezoneA;
+                    } else {
+                        addedText = `Časový posun mezi destinací A a B nelze určit.`;
+                        App.result.append(addedText);
+                    }
+                    if (difference === 1) {
+                        addedText = `Časový posun mezi destinací A a B je ${difference} hodina`;
+                        App.result.append(addedText);
+                    } else if (difference > 1 && difference < 5) {
+                        addedText = `Časový posun mezi destinací A a B jsou ${difference} hodiny`;
+                        App.result.append(addedText);
+                    } else if (difference >= 5) {
+                        addedText = `Časový posun mezi destinací A a B je ${difference} hodin`;
+                        App.result.append(addedText);
+                    }
+                }
 
             }).always(() => {
                 spinner.remove();
             });
         };
 
-        map.getSignals().addListener(window, "map-click", clickMap); //when clicked --> do clickMap
+        map.getSignals().addListener(window, 'map-click', clickMap); //when clicked --> do clickMap
 
     });
 
 });
-
