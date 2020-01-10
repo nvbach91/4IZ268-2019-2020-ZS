89acd32e2ec4ad6599da167edfc51923fbebec0f
AmsiVolv <44711189+AmsiVolv@users.noreply.github.com>
2020-01-10 14:01:05 +0100

    Last version
    Final project version

--- www/volv02/sp2/js/app.js
+++ www/volv02/sp2/js/app.js
@@ -1,4 +1,3 @@
-const apiKey = '21f6de551a459859c11aa47bc21a1d33';
 window.addEventListener('load', () => {
   let long;
   let lat;
@@ -11,6 +10,7 @@ window.addEventListener('load', () => {
   let temperatureSection = document.querySelector('.temperature-degree');
   let temperatureSpan = document.querySelector('.temperature-section span');
   let favoriteCittyFiedld = document.querySelector('.favoriteCity');
+  const apiKey = '21f6de551a459859c11aa47bc21a1d33';
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -56,7 +56,7 @@ window.addEventListener('load', () => {
           window.onpopstate = function (event) {
             // console.log("location: " + location.href + ", state: " + JSON.stringify(event.state));
             var urlState = location.href.split('#');
-            api = `http://api.openweathermap.org/data/2.5/forecast?q=${urlState[urlState.length-1]}&APPID=${apiKey}&cnt=40`;
+            api = `http://api.openweathermap.org/data/2.5/forecast?q=${urlState[urlState.length - 1]}&APPID=${apiKey}&cnt=40`;
             iconWether.innerText = '';
             nextDayInfo.innerText = '';
             getWeather();
@@ -73,10 +73,25 @@ window.addEventListener('load', () => {
             name
           } = data.city;
           timeZone.innerText = (name + ', ' + country);
-          temperatureSection.innerText = kelvinTofahrenheit(data.list[0].main.temp);
-          temperatureSpan.innerHTML = '°F';
 
-          temperatureSection.addEventListener('mouseover', () => {
+
+          if (JSON.parse(localStorage.getItem('unit') || '[]')[0] === '°F') {
+            temperatureSection.innerText = kelvinTofahrenheit(data.list[0].main.temp);
+            temperatureSpan.innerHTML = '°F';
+          } else {
+            temperatureSection.innerText = fahrenheitToCelsius(data.list[0].main.temp);
+            temperatureSpan.innerHTML = '°C';
+          }
+
+
+          $('.temperature-degree').on('mouseover', () => {
+            $('.choseTemp').css("display", "unset");
+            setTimeout(function () {
+              $('.choseTemp').css("display", "none");
+            }, 5000)
+          });
+
+          $('.temperature-degree').off('click').on('click', () => {
             setTimeout(function () {
               if (temperatureSpan.innerText === '°F') {
                 temperatureSpan.innerHTML = '°C';
@@ -105,7 +120,6 @@ window.addEventListener('load', () => {
           data.list.forEach(fiveDayForecast);
 
           function fiveDayForecast(item) {
-            $('.fiveDayInfo').innerText = '';
             let nextDay = item.dt / 86400;
             if (Number.isInteger(nextDay)) {
               //create element wuth class for each day
@@ -121,7 +135,12 @@ window.addEventListener('load', () => {
               oneDay.innerText = nextDayString.toLocaleString('en', {
                 weekday: 'short'
               }) + ', ' + nextDayString.getDate();
-              oneDayTemp.innerHTML = kelvinTofahrenheit(item.main.temp) + ' °F';
+
+              if(localStorage.getItem('unit') === '°F') {
+                oneDayTemp.innerHTML = kelvinTofahrenheit(item.main.temp) + ' °F';
+              } else {
+                oneDayTemp.innerHTML = fahrenheitToCelsius(item.main.temp) + ' °C';
+              }
 
               nextDayInfo.appendChild(oneDay);
               oneDay.appendChild(oneDayTemp);
@@ -143,31 +162,79 @@ window.addEventListener('load', () => {
             };
           };
 
+          tempertureUnit = localStorage.getItem('unit') ? JSON.parse(localStorage.getItem('unit')) : [];
+          $('#iWant°C').on('click', () => {
+            addToLocalStorageUnit($('#iWant°C'));
+          });
+          $('#iWant°F').on('click', () => {
+            addToLocalStorageUnit($('#iWant°F'));
+          });
+
+          function addToLocalStorageUnit(a) {
+            unitOfTemp = a.attr('id').slice(5, 7);
+            if (tempertureUnit.includes(unitOfTemp, 0)) {
+              return false;
+            } else {
+              localStorage.setItem('unit', unitOfTemp);
+              console.log(tempertureUnit);
+              if(localStorage.getItem('unit') === '°F') {
+                temperatureSection.innerText = kelvinTofahrenheit(data.list[0].main.temp);
+                temperatureSpan.innerHTML = '°F';
+              } else {
+                temperatureSection.innerText = fahrenheitToCelsius(data.list[0].main.temp);
+                temperatureSpan.innerHTML = '°C';
+              }
+              localStorageFunction();
+              $('.fiveDayInfo').empty();
+              data.list.forEach(fiveDayForecast);
+            }
+          };
+
           //add to local storage
           //key 'city' with value of a name of the city
-          let citiesArray = localStorage.getItem('city') ? JSON.parse(localStorage.getItem('city')) : [];
+          citiesArray = localStorage.getItem('city') ? JSON.parse(localStorage.getItem('city')) : [];
           if (localStorage.getItem('city') != null) {
             localStorageFunction();
           } else {
             favoriteCittyFiedld.innerText = 'The list of your favorite cities is empty.';
           };
-          $('.favorite').on('click', () => {
-            if (citiesArray.includes(name, 0) === true) {
-              return false;
-            } else {
-              if (citiesArray.length < 5) {
-                citiesArray.push(name);
-                localStorage.setItem('city', JSON.stringify(citiesArray));
-                localStorageFunction();
-              } else {
-                alert('You can add only 5 cities to your favorite city list.');
-              }
-            }
-          });
+
+          if (citiesArray.includes(name, 0)) {
+            $('.addRemoveBtn').attr('src', './img/icons8-star-16-remove.png');
+            $('.favorite').off('click').on('click', () => {
+              citiesArray.splice(citiesArray.indexOf(name), 1);
+              localStorage.removeItem('city');
+              localStorage.setItem('city', JSON.stringify(citiesArray));
+              $('.addRemoveBtn').attr('src', './img/icons8-star-16-add.png');
+              localStorageFunction();
+            });
+          } else {
+            $('.addRemoveBtn').attr('src', './img/icons8-star-16-add.png');
+            $('.favorite').off('click').on('click', () => {
+              api = `http://api.openweathermap.org/data/2.5/forecast?q=${name}&APPID=${apiKey}&cnt=40`
+              fetch(api).then(function (response) {
+                if (response.status != 200) {
+                  throw 'Not found'
+                } else {
+                  if (citiesArray.includes(name, 0)) {
+                    return false;
+                  } else {
+                    if (citiesArray.length < 5) {
+                      citiesArray.push(name);
+                      localStorage.setItem('city', JSON.stringify(citiesArray));
+                      localStorageFunction();
+                      $('.addRemoveBtn').attr('src', './img/icons8-star-16-remove.png');
+                    } else {
+                      alert('You can add only 5 cities to your favorite city list.');
+                    };
+                  };
+                };
+              }).catch(function (a) { alert(a) });
+            });
+          };
 
           function localStorageFunction() {
             favoriteCittyFiedld.innerText = '';
-            console.log(citiesArray);
             citiesArray.forEach(name => {
               //get a weather of this city
               api = `http://api.openweathermap.org/data/2.5/forecast?q=${name}&APPID=${apiKey}&cnt=40`;
@@ -179,12 +246,22 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                   $('.loader').remove();
-                  var html = `<div class="thisCityStorage">
+                  var html = '';
+                  if(localStorage.getItem('unit') === '°F') {
+                    html = `<div class="thisCityStorage">
                                  <a href="#${name}"><p>${name}</p></a>
                                  <div class="tempForFavoriteCitty">${kelvinTofahrenheit(data.list[0].main.temp)}<span> °F</span></div>
                                  <img alt="WeatherIcon_For_${name}" src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png">
                                  <div class="descriptionOfIconWeather">${ucFirst(data.list[0].weather[0].description)}</div> 
+                              </div>`;
+                  } else {
+                    html = `<div class="thisCityStorage">
+                                 <a href="#${name}"><p>${name}</p></a>
+                                 <div class="tempForFavoriteCitty">${fahrenheitToCelsius(data.list[0].main.temp)}<span> °C</span></div>
+                                 <img alt="WeatherIcon_For_${name}" src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png">
+                                 <div class="descriptionOfIconWeather">${ucFirst(data.list[0].weather[0].description)}</div> 
                               </div>`
+                  }
                   $('.favoriteCity').append(html);
                 });
             });
@@ -235,10 +312,10 @@ window.addEventListener('load', () => {
             }, 125);
             setTimeout(function () {
               $('.citySearchSuggestion').remove();
-            }, 750000);
+            }, 7500);
             setTimeout(function () {
               $('.searchInput').val('');
-            }, 750000);
+            }, 7500);
           });
       });
     }
