a8e89582321b6b20a8e7ec429b184a2632217a61
frim00 <57260961+frim00@users.noreply.github.com>
2020-01-10 18:12:11 +0100

    Odstranění přebytečných souborů

--- www/frim00/sp2/src/twitterConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'cEmAwP4JpvEpywMhkwLv74j3i',
-  consumer_secret: 'CMO6EpfI00PsNy3Aw6BIO7csXJzUH45JHz4KIBRSCqdZS2QxmH',
-  access_token: '2364091712-emchSYoQ9qE5MHCXuzMPJ6goXl3uvrVKMEfodDn',
-  access_token_secret: 'aZlSrf5Quy6pbgjyNV00AwFMkMzycufeHZ41Y98kkmF4O',
-};
