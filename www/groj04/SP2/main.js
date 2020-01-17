496c77cdb5deeb3ba4d5fb6a76bc4b4b8798ada1
Gron1k <58661689+Gron1k@users.noreply.github.com>
2020-01-17 03:41:25 +0100

    vylepšená "dokončená" verze
    v této verzi jsem se snažil opravit nějaké nedostatky, na které jste poukázal

--- www/groj04/SP2/main.js
+++ www/groj04/SP2/main.js
@@ -1,196 +1,223 @@
 /**
  * uložení email do local storage
  */
+$(document).ready(function() {
+  $("#confirm").click(function removeFunction() {
+    var mail = $("#mail").val();
+    localStorage.setItem("mail", mail);
+    console.log(localStorage.getItem("mail"));
+    $("main").remove();
+    /**
+ *     $('#main').compare(function(){ 
+    text = document.getElementById('mail').innerHTML
+    var success = document.getElementById('tryout').innerHTML = /[a-z0-9._%+-]+@gmail.com/.test(text)
+    if (success !== true) {
+      alert("Invalid gmail input.");
+    } else {
+    }
+    }) 
+ * 
+ */
+  });
+});
 
-$('#confirm').click(function(){
-    var mail = $('#mail').val()
-    localStorage.setItem('mail', mail)
-    $('main').remove()
-    console.log(localStorage.getItem('mail'))
-})
 /**
  * vytvoření třídy kalkulačka s funkcemi
+ * zobrazuje text elementy na displayi
+ * prev = prev protože určujeme že prev je ta konstanta prev
+ * this.clear protože hned na začátku nechceme žádné proměnné ani operace
  */
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  /**
+   * Zobrazuje cisla v "displayi"
+   *
+   */
+  displayNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  /**
+   * určuje jaká se provede operace
+   * this.operation ukazuje na operaci z compute
+   */
+  whichOperation(operation) {
+    if (!this.currentOperand) return;
+    if (this.previousOperand !== "") {
+      this.compute();
     }
 
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+  /**
+   * udává operaci
+   * if is nan nepůstí rovnítko když není operace kompletní jinak se to pokazí
+   */
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev / current;
+        break;
+      case "x^y":
+        computation = Math.pow(prev, current);
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
     /**
-     * Zobrazuje cisla v "displayi"
+     * výsledek (computation) do local storage
      */
-    displayNumber(number) {
-        if (number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number.toString()
+    localStorage.setItem("result", computation);
+    console.log(localStorage.getItem("result"));
+  }
+  /**
+   * pomáhá se zobrazením desetinných čísel
+   */
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0
+      });
     }
-/**
- * určuje jaká se provede operace
- */
-    whichOperation(operation) {
-
-        if (this.currentOperand === '') return
-        if (this.previousOperand !== '') {
-            this.compute()
-        }
-       
-        this.operation = operation
-        this.previousOperand = this.currentOperand
-        this.currentOperand = ''
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
     }
-/**
- * udává operaci
- */
-    compute() {
-        let computation
-        const prev = parseFloat(this.previousOperand)
-        const current = parseFloat(this.currentOperand)
-        if (isNaN(prev) || isNaN(current)) return
-        switch (this.operation) {
-            case '+':
-                computation = prev + current
-                break
-            case '-':
-                computation = prev - current
-                break
-            case '*':
-                computation = prev * current
-                break
-            case '/':
-                computation = prev / current
-                break
-            case 'x^y':
-                computation = Math.pow(prev, current)
-                break
-            default:
-                return
-        }
-        this.currentOperand = computation
-        this.operation = undefined
-        this.previousOperand = ''
-        /**
-         * výsledek (computation) do local storage
-         */
-        localStorage.setItem('result', computation)
-        console.log(localStorage.getItem('result'))
+  }
+  /**
+   * aktualizuje display podle zadaných hodnot
+   */
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
-/**
- * pomáhá se zobrazením desetinných čísel
- */
-    getDisplayNumber(number) {
-        const stringNumber = number.toString()
-        const integerDigits = parseFloat(stringNumber.split('.')[0])
-        const decimalDigits = stringNumber.split('.')[1]
-        let integerDisplay
-        if (isNaN(integerDigits)) {
-          integerDisplay = ''
-        } else {
-          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-        }
-        if (decimalDigits != null) {
-          return `${integerDisplay}.${decimalDigits}`
-        } else {
-          return integerDisplay
-        }
-      }
-/**
- * aktualizuje display podle zadaných hodnot
- */
-    updateDisplay() {
-        this.currentOperandTextElement.innerText =
-          this.getDisplayNumber(this.currentOperand)
-        if (this.operation != null) {
-          this.previousOperandTextElement.innerText =
-            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-        } else {
-          this.previousOperandTextElement.innerText = ''
-        }
-      }
-      /**
-       * smaže zdané hodnoty
-       */
-    clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
-    }
-/**
- * smaže jedno poslední zadané aktuální číslo
- */
-    delete() {
-        this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
+  }
+  /**
+   * smaže zdané hodnoty
+   */
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  /**
+   * smaže jedno poslední zadané aktuální číslo
+   */
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 }
 
 /**
  * zadaní konstant z buttonů
  */
 
-const numberButtons = document.querySelectorAll('#numButton')
-const operationButtons = document.querySelectorAll('#opButton')
-const equalsButton = document.querySelector('#eqButton')
-const deleteButton = document.querySelector('#delButton')
-const allClearButton = document.querySelector('#all-clear')
-const previousOperandTextElement = document.querySelector('#prev-opButton')
-const currentOperandTextElement = document.querySelector('#cur-opButton')
-
+const numberButtons = document.querySelectorAll("#numButton");
+const operationButtons = document.querySelectorAll("#opButton");
+const equalsButton = document.querySelector("#eqButton");
+const deleteButton = document.querySelector("#delButton");
+const allClearButton = document.querySelector("#all-clear");
+const previousOperandTextElement = document.querySelector("#prev-opButton");
+const currentOperandTextElement = document.querySelector("#cur-opButton");
 
 /**
  * funkčnost kalkulačky
  * každý button provede určitou operaci spojenou s funkcemi
  */
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.displayNumber(button.innerText)
-        calculator.updateDisplay()
-    })
-})
+  button.addEventListener("click", () => {
+    calculator.displayNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
 operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.whichOperation(button.innerText)
-        calculator.updateDisplay()
-    })
-})
+  button.addEventListener("click", () => {
+    calculator.whichOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-equalsButton.addEventListener('click', button => {
-    calculator.compute()
-    calculator.updateDisplay()
-})
+equalsButton.addEventListener("click", button => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
 
-allClearButton.addEventListener('click', button => {
-    calculator.clear()
-    calculator.updateDisplay()
-})
+allClearButton.addEventListener("click", button => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
-deleteButton.addEventListener('click', button => {
-    calculator.delete()
-    calculator.updateDisplay()
-})
+deleteButton.addEventListener("click", button => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
 
 // Client ID and API key from the Developer Console
-var CLIENT_ID = 1031527171533-l8ui4balolna0o8r5126tgh21qjni788.apps.googleusercontent.com;
+var CLIENT_ID =
+  1031527171533 - l8ui4balolna0o8r5126tgh21qjni788.apps.googleusercontent.com;
 var API_KEY = AIzaSyDYh5v3oIGPq0JZSzyCmX3O3TvcVoD4vmM;
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+var DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
+];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+var SCOPES = "https://www.googleapis.com/auth/drive.metadata.readonly";
 
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
+var authorizeButton = document.getElementById("authorize_button");
+var signoutButton = document.getElementById("signout_button");
 
 /**
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+  gapi.load("client:auth2", initClient);
 }
 
 /**
@@ -198,22 +225,27 @@ function handleClientLoad() {
  *  listeners.
  */
 function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    })
+    .then(
+      function() {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+      },
+      function(error) {
+        appendPre(JSON.stringify(error, null, 2));
+      }
+    );
 }
 
 /**
@@ -222,12 +254,12 @@ function initClient() {
  */
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
+    authorizeButton.style.display = "none";
+    signoutButton.style.display = "block";
     listFiles();
   } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
+    authorizeButton.style.display = "block";
+    signoutButton.style.display = "none";
   }
 }
 
@@ -252,8 +284,8 @@ function handleSignoutClick(event) {
  * @param {string} message Text to be placed in pre element.
  */
 function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
+  var pre = document.getElementById("content");
+  var textContent = document.createTextNode(message + "\n");
   pre.appendChild(textContent);
 }
 
@@ -261,19 +293,21 @@ function appendPre(message) {
  * Print files.
  */
 function listFiles() {
-  gapi.client.drive.files.list({
-    'pageSize': 10,
-    'fields': "nextPageToken, files(id, name)"
-  }).then(function(response) {
-    appendPre('Files:');
-    var files = response.result.files;
-    if (files && files.length > 0) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        appendPre(file.name + ' (' + file.id + ')');
+  gapi.client.drive.files
+    .list({
+      pageSize: 10,
+      fields: "nextPageToken, files(id, name)"
+    })
+    .then(function(response) {
+      appendPre("Files:");
+      var files = response.result.files;
+      if (files && files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          appendPre(file.name + " (" + file.id + ")");
+        }
+      } else {
+        appendPre("No files found.");
       }
-    } else {
-      appendPre('No files found.');
-    }
-  });
+    });
 }
