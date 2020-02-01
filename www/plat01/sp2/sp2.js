e46aa79b8e713a7b5482b3d3627902c1c1089b09
Nguyen Viet Bach <nvbach91@outlook.com>
2020-02-01 11:39:04 +0100

    moved files

--- www/plat01/sp2/sp2.js
+++ www/plat01/sp2/sp2.js
@@ -10,19 +10,14 @@ App.init = () => {
     App.clearPickerButton = $('#clearPicker-button');
     App.clearFavouriteButton = $('#clearFavourite-button');
     App.showBooksButton = $('#show-books');
-    
-    App.spinerBody = $('#spiner-body');
 };
 
 function showBooklist() {
 
-    const spinner = $('<div class="spinner"></div>');
-    $('#spiner-body').prepend(spinner);
-
     //make empty
-    if (favouriteBooks.length) {
+    if (favouriteBooks.length){
         App.bookList.empty();
-        for (let i = 0; i < favouriteBooks.length; i++) {
+        for (let i = 0; i < favouriteBooks.length; i++){
 
             let book = favouriteBooks[i];
             //put book into booklist
@@ -34,14 +29,11 @@ function showBooklist() {
 
         App.bookListLabel.hide();
         App.clearFavouriteButton.show();
-       
+
     }
-        
-    
 }
 
-function pickerItemExist(keyword) {
-
+function pickerItemExist(keyword){
     //Check existing books
     const existingBooks = App.picker.children();
     for (let j = 0; j < existingBooks.length; j++) {
@@ -52,14 +44,12 @@ function pickerItemExist(keyword) {
         }
     }
     return false;
-
 }
 
-
 function createPickerItem(book) {
-   
+
     let item = $('<li class="picker-item">');
-    let bookKey = book.title.replace(/ /g, '')
+    let bookKey = book.title.replace(/ /g,'')
     item.attr('data-name', bookKey);
     item.html('<b>' + book.title + '</b>');
 
@@ -77,23 +67,19 @@ function createPickerItem(book) {
         addToFavouriteBtnHandler(book);
     });
     button2.appendTo(item);
-    
+
     return item;
 
-   
 }
 
-
-function fillPicker(data) {
-
-    
+function fillPicker (data){
 
     for (let i = 0; i < data.docs.length; i++) {
         //console.log(resp.docs);
 
         // vytvořit i-tou knížku k vybrání
         let book = data.docs[i];
-        if (pickerItemExist(book.title)) {
+        if (pickerItemExist(book.title)){
             console.log("Book ", book.title, " is already in the picker...");
             continue;
         }
@@ -101,60 +87,44 @@ function fillPicker(data) {
         item.appendTo(App.picker);
 
         App.picker.appendTo('body');
-        
     }
-
-    
 }
 
 //Function that is called when Searched button is clicked
-//asks server to get data
-//fills the picker
-function searchBtnHandler() {
-
-    
+    //asks server to get data
+    //fills the picker
+function searchBtnHandler(){
 
     let keyword = App.bookNameInput.val();
     keyword = keyword.toLowerCase().trim();
     if (!keyword) {
         return alert('Please enter a valid book or author name');
     }
-    console.log("Keyword typed: ", keyword);
+    console.info("Keyword typed: ", keyword);
 
-    
 
-    var searchMode = $('#bb').val();
-    if (searchMode === 'title'){
-        
-    const spinner = $('<div class="spinner"></div>');
-    $('#spiner-body').prepend(spinner);
-    
-        $.get(`http://openlibrary.org/search.json?title=${keyword}/`).done((resp) => {
-            console.log(resp);
-            fillPicker(resp);
-            spinner.remove();
-        });
-    }
-    else if (searchMode === 'author')
-
+    //Async call to the openlibrary server
+    //try to find by title
+    $.get(`http://openlibrary.org/search.json?title=${keyword}/`).done((resp) => {
+        console.log(resp);
+        fillPicker(resp);
+    });
+    //try to find by author name
     $.get(`http://openlibrary.org/search.json?author=${keyword}/`).done((resp) => {
         console.info(resp);
         fillPicker(resp);
-        spinner.remove();
     });
-    
-   
 }
 
 //Function that is called when Clear Searched button is clicked
-//clears all searched books
+    //clears all searched books
 function clearPickerBtnHandler() {
 
     App.picker.empty();
 }
 
 //Function that is called when Clear Favourite button is clicked
-//clears all saved favourite books
+    //clears all saved favourite books
 function clearFavouriteBtnHandler() {
 
     favouriteBooks = [];
@@ -166,20 +136,20 @@ function clearFavouriteBtnHandler() {
 }
 
 //Function that is called when Info button is clicked
-//replaces row in picker and shows more info
+    //replaces row in picker and shows more info
 function infoBtnHandler(book) {
 
     console.log(book);
 
-    let bookKey = book.title.replace(/ /g, '');
+    let bookKey = book.title.replace(/ /g,'');
     let bookItem = $('.picker-item[data-name=' + bookKey + ']')[0];
     let string = 'TITLE: ' + book.title;
-    string += '\nAUTHOR: ' + (book.author_name) ? book.autor_name : '';
-    string += '\nYEAR: ' + (book.first_publish_year) ? book.first_publish_year : '';
-    if (book.isbn) {
+    string += '\nAUTHOR: ' + (book.author_name)? book.autor_name : '';
+    string += '\nYEAR: ' + (book.first_publish_year)? book.first_publish_year : '';
+    if (book.isbn){
         string += '\nISBN: ' + book.isbn[0];
     }
-    if (book.subject) {
+    if (book.subject){
         string += '\nKEYWORD: ' + book.subject[0];
     }
     $(bookItem).html(string);
@@ -197,7 +167,7 @@ function infoBtnHandler(book) {
 //Function that is called when Add To Favourite button is clicked
 function addToFavouriteBtnHandler(book) {
 
-    if (favouriteBooks.includes(book)) {
+    if (favouriteBooks.includes(book)){
         alert("Book has already been saved to Favourites...");
     }
     else {
@@ -207,7 +177,7 @@ function addToFavouriteBtnHandler(book) {
         showBooklist();
 
         //save cookie cookie
-        $.cookie('booklist', JSON.stringify(favouriteBooks), { path: '/' });
+        $.cookie('booklist', JSON.stringify(favouriteBooks), {path: '/'});
 
         alert("Book was saved to Favourites...");
     }
@@ -236,69 +206,14 @@ $(document).ready(() => {
     $.cookie('name', 'value', { path: '/' });
     let localStorageTemp = $.cookie('booklist');
 
-    if (localStorageTemp) {
+    if (localStorageTemp){
         favouriteBooks = JSON.parse(localStorageTemp);
         showBooklist();
     }
-    else {
+    else{
         App.clearFavouriteButton.hide();
     }
     console.info("Application Inited");
-
-
-   
-
-   /* let favouriteBooks_serialized = JSON.stringify(fovouriteBooks)
-
-   letStorage.setItem("favouriteBooks", favouriteBooks_serialized)
-
-    let favouriteBooks_deserialized = localStory.getItem("favouriteBooks")*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /*
-   function addToFavoriteBooks(keyword){
-       let bookList = JSON.parse(localStorage.getItem("favoriteBooks"));
-       bookList.push();
-       localStorage.setItem("favoriteBooks", JSON.stringify(bookList));
-   }
-   
-   function removeFromFavorites(){
-       let bookList = JSON.parse(localStorage.getItem("favoriteBooks"));
-       bookList.splice();
-       localStorage.setItem("favoriteBooks", JSON.stringify(bookList));
-   }
-   
-   function isInFavorites() {
-    let bookList = JSON.parse(localStorage.getItem("favoriteBooks"));
-    return bookList.includes();
-}
-   
-   
-   
-   
-   
-   */
 });
 
 
-
-/*1- spinner
-2 - local storage
-3- podmínka na vyhledávání - autor/title
-4- předělat rendrování knih v cyklu
-
-var searchMode = ('').val();
-if (searModde === ¨title'){
-
-}else if (searchMode === 'author')*/
